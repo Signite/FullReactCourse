c2539397a89ec87eaec52aec6ac6575d5e4d9bca
Signite <sistemsignite@gmail.com>
2022-01-09 20:41:10 +0500

    Start practice one

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
 "use strict";
 
-// alert("Hello");
+const numberOfFilms = prompt('How many films you saw?','');
 
-// const result = confirm("Do you want to go to the sea");
-// alert(result);
-
-// const answer = +prompt("Which sea do you want to go to?","Black sea");
-// alert(answer + ' ' + typeof(answer));
-
-// const answers = [];
-
-// answers[0] = prompt("What is your name?");
-// answers.push(prompt("How old are you?", "18"));
-// answers[3] = prompt("Where are aoy from?");
-
-// document.write(answers);
-
-// const category = "toys";
-
-// console.log(`https://someurl.com/${category}`);
-
-// const user = "Ivan";
-
-// alert(`Hi, ${user}`);
-
-console.log(!"w");
